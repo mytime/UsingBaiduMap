d6efa61f76af1a639e9b9daf9c8bd119172364a7
Administrator <cmba@qq.com>
2016-02-25 15:31:17 +0800

    判断网络和Key状态

--- app/src/main/java/com/jikexueyuan/usingbaidumap/MainActivity.java
+++ app/src/main/java/com/jikexueyuan/usingbaidumap/MainActivity.java
@@ -1,7 +1,12 @@
 package com.jikexueyuan.usingbaidumap;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.MapView;
@@ -9,25 +14,53 @@ import com.baidu.mapapi.map.MapView;
 public class MainActivity extends AppCompatActivity {
 
     private MapView mMapView = null;
+    private MyBaiduSdkReceiver baiduSdkReceiver = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initManager();
+        setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.bmapView);
 
+
+    }
+
+    private void initManager() {
         //在使用SDK各组件之前初始化context信息，传入ApplicationContext
         //注意该方法要再setContentView方法之前实现
         SDKInitializer.initialize(getApplicationContext());
 
-        setContentView(R.layout.activity_main);
-        //获取地图控件引用
-        mMapView = (MapView) findViewById(R.id.bmapView);
+        baiduSdkReceiver = new MyBaiduSdkReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(SDKInitializer.SDK_BROADCAST_ACTION_STRING_NETWORK_ERROR);
+        intentFilter.addAction(SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_ERROR);
+
+        registerReceiver(baiduSdkReceiver,intentFilter);
+    }
+
+    class MyBaiduSdkReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String result = intent.getAction();
+            if (result.equals(SDKInitializer.SDK_BROADCAST_ACTION_STRING_NETWORK_ERROR)){
+                Toast.makeText(getApplicationContext(),"无网络",Toast.LENGTH_SHORT).show();
+            }else if (result.equals(SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_ERROR)){
+                Toast.makeText(getApplicationContext(),"Key错误",Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 
     @Override
     protected void onDestroy() {
+        unregisterReceiver(baiduSdkReceiver);
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mMapView.onDestroy();
+
     }
 
     @Override
